3e82096834ea8a5422be3348b240c8b1cdcfa7b2
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2023-04-23 01:52:56 -0600

    port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import db from './db/db.js';
 import routes from './routes/index.js';
 import cors from 'cors'
 
-const PORT=process.env.API_PORT || 3000;
+const port =process.env.PORT || 3000;
 const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -18,8 +18,8 @@ app.use(morgan('tiny'))
 
 app.use('/api', routes)
 await db.sync({force:false}).then(()=>{
-    app.listen(PORT,'0.0.0.0', ()=>{
-        console.log("servidor corriendo", PORT)
+    app.listen(port ,'0.0.0.0', ()=>{
+        console.log("servidor corriendo", port )
     })
 })
 
