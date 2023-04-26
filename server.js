d285675dbfa649308a586d64e52b9fac9b5b9d9b
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2023-04-26 13:11:36 -0600

    C:/Program Files/Git/server se modifica el cors

--- server.js
+++ server.js
@@ -12,8 +12,13 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 // middlewares de terceros
-app.use(cors())
+app.use(cors({
+    origin:'http://127.0.0.1:5173',
+    credentials:true,
+    allowedHeaders:true
+}))
 app.use(cookieParser())
+
 app.use(morgan('tiny'))
 
 app.use('/api', routes)
