c15e62b0c14bf057802d35d9bfa6acb4bc59a1d4
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2023-04-26 15:17:29 -0600

    prueba de cookies

--- controllers/userController.js
+++ controllers/userController.js
@@ -230,8 +230,8 @@ class UserController {
                 imgProfile:results.imgProfile
             }
             const token=generateToken(payload)
-            res.cookie("token", token)
-            res.cookie("tuptm", "tuptm")
+            res.cookie("token", token, {path:'/'})
+            res.cookie("tuptm", "tuptm", {path:'/'})
             res.status(200).send({message:"Usuario logeado", success:true})
         } catch (error) {
             return res.status(400).send({
@@ -261,7 +261,7 @@ class UserController {
     static async logOut(req, res){
         console.log("logout")
         try {
-            res.clearCookie("token");
+            res.clearCookie("token", {path:'/'});
             res.send(204)
         } catch (error) {
             return res.status(500).send({
