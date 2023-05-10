672eb1a7df180356c98fcf165f1eb14836049c6b
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2023-05-10 10:12:26 -0600

    C:/Program Files/Git/server.js lineas 11 y 12, update a bodyparse.json y bodyparser.urlencoded

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ import cors from 'cors'
 
 const port =process.env.PORT || 3000;
 const app=express()
-app.use(bodyParser.json({limit: '50mb', extended:true}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
+app.use(express.json({limit: '50mb', extended:true}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
 // app.use(express.json())
 // app.use(express.urlencoded({extended:true}))
 const allowedOrigins = ['https://techzone-pi.vercel.app', 'http://127.0.0.1:5173', 
