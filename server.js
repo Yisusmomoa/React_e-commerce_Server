ad47b191ff661044b7717deaa693b16b91109a31
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2023-05-10 10:24:08 -0600

    instalación de body-parser

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ import cookieParser from 'cookie-parser';
 import db from './db/db.js';
 import routes from './routes/index.js';
 import cors from 'cors'
+import bodyParser from 'body-parser';
 
 const port =process.env.PORT || 3000;
 const app=express()
-app.use(express.json({limit: '50mb', extended:true}));
-app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
+app.use(bodyParser.json({limit: '50mb', extended:true}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
 // app.use(express.json())
 // app.use(express.urlencoded({extended:true}))
 const allowedOrigins = ['https://techzone-pi.vercel.app', 'http://127.0.0.1:5173', 
