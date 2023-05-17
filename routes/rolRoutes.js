97df01f9e0a89cb767085a98182810e31befc9dc
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2023-05-17 16:33:51 -0600

    showRols error

--- routes/rolRoutes.js
+++ routes/rolRoutes.js
@@ -1,10 +1,9 @@
 import express from 'express'
 import RolController from '../controllers/rolController.js'
 
-
-
 const rolRoutes=express.Router()
 
 rolRoutes.post('/', RolController.creteRol)
+rolRoutes.get('/', RolController.showRols)
 
 export default rolRoutes
